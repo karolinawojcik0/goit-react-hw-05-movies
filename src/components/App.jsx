f9c351dcb225c06037e84665ddb3fd3c62c967fc
Karolina <143939666+karolinawojcik0@users.noreply.github.com>
2024-05-01 13:07:48 +0200

    first

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,12 @@
+import { Routes, Route } from 'react-router-dom';
+import { Home } from './Home/Home';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Routes>
+        <Route path="/" exact component={Home} />
+      </Routes>
+    </>
   );
 };
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export const Home = () => {
+  const [trendingMovies, setTrendingMovies] = useState([]);
+
+  useEffect(() => {
+    getTrendingMovies();
+  }, []);
+
+  const getTrendingMovies = async () => {
+    try {
+      const response = await axios.get(
+        'https://api.themoviedb.org/3/trending/movie/day',
+        {
+          params: {
+            api_key: 'ab4b5bd9c7ffd7f3aa53eea7788a929e',
+          },
+        }
+      );
+      setTrendingMovies(response.data.results);
+    } catch (error) {
+      console.error('Error fetching trending movies:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Trending Movies</h2>
+      <ul>
+        {trendingMovies.map(movie => (
+          <li key={movie.id}>{movie.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
